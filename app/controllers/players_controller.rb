3246757e84a936f678dca2c9d1ee49e3a06e4263
Bruce White <bruce@homecca.com>
2013-10-14 20:51:24 -0700

    Add simple sorting to players#index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,8 +2,9 @@ class PlayersController < ApplicationController
   respond_to :html
 
   def index
+    @o = params[:o] || 'AVG DESC'
     @season = Season.where(year: 1998).first
-    @players = @season.players.limit(100)
+    @players = @season.players.order(@o).limit(100)
     respond_with(@players)
   end
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,6 @@
 module PlayersHelper
+  def column_sort_tag(column)
+    order = (@o == column + ' DESC') ? column + ' ASC' : column + ' DESC'
+    link_to column, players_path(o: order)
+  end
 end
