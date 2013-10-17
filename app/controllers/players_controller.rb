5d6c883b0450844f587a38922ff8698e31dfe471
Bruce White <bruce@homecca.com>
2013-10-16 21:01:06 -0700

    Include teams in players#index query

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,7 +4,7 @@ class PlayersController < ApplicationController
   def index
     @order = params[:order] || 'AVG DESC'
     @season = Season.from_param(params[:season_id])
-    @players = @season.players.order(@order).page params[:page]
+    @players = @season.players.includes(:team).order(@order).page params[:page]
     respond_with(@players)
   end
 end
